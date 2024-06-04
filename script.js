e914b8516cd93dcdd0be11f176183cb95bf3dbb4
Roby1kenoby <r0by1kenoby@hotmail.it>
2024-06-04 20:48:08 +0200

    added some graphic improvements (still looks like shit)

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ let showCards = function(arr){
 // creazione di una card
 let createCard = function(book){
     let cardWrapper = document.createElement('div')
-    cardWrapper.className = 'cardWrapper col'
+    cardWrapper.className = 'cardWrapper'
     let card = document.createElement('div')
     card.className = 'myCard'
     card.dataset.asin = book.asin
@@ -34,7 +34,7 @@ let createCard = function(book){
     card.dataset.price = book.price
     card.innerHTML = 
     `
-    <div class="imgWrapper">
+    <div class="imgWrapper d-flex">
         <img src="${book.img}" class="album-img card-img-top" alt="${book.title}">
     </div>
     <div class="myCardBody">
@@ -43,9 +43,8 @@ let createCard = function(book){
             <button class="btn btn-danger d-none" onClick="toggleCart(this.parentElement.parentElement.parentElement)">Remove</button>
         </div>
         <div class="bottomInfo">
-                <h5 class="title card-title text-truncate">${book.title}</h5>
-                <p class="genre">${book.category}</p>
-                <p class="price">${book.price}€</p>
+                <p class="title card-title">${book.title}</h5>
+                <p class="genreAndPrice"><span>${book.category}</span> <span>${book.price}€</span></p>
         </div>
     </div>
     `
@@ -123,10 +122,15 @@ let addRowToCart = function(card)
 {
     let le = document.createElement('li')
     le.innerHTML =
-    `<div class="d-flex justify-content-between align-baseline">
-        <p class="m-0">${card.dataset.title}</p>
-        <p class="m-0 listPrice">${card.dataset.price}€</p>
-        <button onClick="removeRowFromCart(this.parentElement.parentElement, '${card.dataset.asin}')">elimina</button>
+    `<div class="cartElement">
+        <p>${card.dataset.title}</p>
+        <p class="listPrice">${card.dataset.price}€</p>
+        <button class="btn btn-danger" onClick="removeRowFromCart(this.parentElement.parentElement, '${card.dataset.asin}')">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+            </svg>
+        </button>
     </div>
     `
     // aggiungo l'attributo asin perchè lo usero per eliminare l'elemento li se
